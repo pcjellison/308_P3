5fcde57d615c5ffa954f7746483c854d2e0942db
pcjellison <pcjellison@ksu.edu>
2017-12-06 13:07:47 -0600

    12/6

--- JellisonP/Main.cpp
+++ JellisonP/Main.cpp
@@ -4,57 +4,31 @@
 #include "Set.cpp"
 using namespace std;
 
+void getInput(string, Set<int>*);
 int main()
 {
 	Set<int> *firstSet = new Set<int>();
 	Set<int> *secondSet = new Set<int>();
 
-	//int arraySize = 2;
-	//int* firstArray = { };
-	//int* secondArray = new int[10];
-	string line;
-	char input[50];
+	string input;
 
 	cout << "Please input the first set of numbers (EX: 4, 2, 3, 1, 5)" << endl;
-	cin >> line;
+	cin >> input;
 
-	for (int i = 0; i < 5; i++)
+	getInput(input, *firstSet);
+}
+
+void getInput(string in, Set<int> &set)
+{
+	string delimiter = ", ";
+
+	int pos = 0;
+	string token;
+	while ((pos = in.find(delimiter)) != -1)
 	{
-		input[i] = line[i];
+		token = in.substr(0, pos);
+		set.add(stoi(token));
+		in.erase(0, pos + delimiter.length());
 	}
-
-	cout << input;
-
-	//string delimiter = ", ";
-
-	//int pos = 0;
-	//string token;
-	//while ((pos = input.find(delimiter)) != string::npos)
-	//{
-	//	token = input.substr(0, pos);
-	//	firstSet->add(stoi(token));
-	//	input.erase(0, pos + delimiter.length());
-	//}
-	//firstSet->add(stoi(token));
-
-	char *next = NULL;
-	int num;
-	char *c = NULL;
-	c = strtok_s(input, ", ", &next);
-	num = atoi(c);
-	firstSet->add(num);
-	cout << "before" << endl;
-	while (c != NULL)
-	{
-		cout << c << endl;
-		c = strtok_s(NULL, ", ", &next);
-		num = int(c);
-		cout << num << endl;
-		firstSet->add(num);
-	}
-	cout << "out of while loop" << endl;
-	firstSet->print();
-
-	int end;
-	cin >> end;
+	set.add(stoi(token));
 }
